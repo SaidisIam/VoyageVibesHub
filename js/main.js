b990bdf553fc7631afbce63b593a919941f2ab35
saidislam2008 <saidislammamasaliev@gmail.com>
2023-12-07 01:09:43 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+customSelect('select');
+
+const picker = new easepick.create({
+	element: document.getElementById('date'),
+	css: ['https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css'],
+	format: 'DD-MM-YY',
+});
+
+/* Subscribe Label */
+
+const inputSubscribe = document.querySelector('.subscribe__input')
+const label = document.querySelector('.subscribe__label')
+inputSubscribe.addEventListener('input', ()=> {
+	if(inputSubscribe.value.trim() != '') {
+		label.classList.add('subscribe__label--top');
+	} else {
+		label.classList.remove('subscribe__label--top')
+	}
+})
+
+
+// const picker = new easypick.create({
+//     // lang: 'ru-Ru',
+//     // lang: 'en-En',
+//     element: document.getElementById('date'),
+//     css: [
+//         'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
+//     ]
+//     // format: 'DD.MM.YY', 
+// })
